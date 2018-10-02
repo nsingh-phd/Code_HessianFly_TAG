58ffcccf462338bb5094e831db68e23856378f15
Narinder Singh <nss470@ksu.edu>
2018-10-02 15:39:18 -0500

    working  first file

--- Analysis_AlleleCount.R
+++ Analysis_AlleleCount.R
@@ -1,40 +1,42 @@
 ## ###########################
 ## Hessian Fly TAG Manuscript
 ## Author - Narinder Singh
+## Mapping with Allele counts
 ## ###########################
 
-# load required packages
-library(pacman)
-p_load(data.table)
+# load required functions, files, and packages
+  chrom.info <- read.table('data/chrom_info.txt', header = T, as.is = T)
+  source("functions.R")
 
-# read hapfile
-allele.count <- fread("data/AlleleCount.txt", header = T, check.names = F, data.table = F)
-colnames(allele.count) # check column names
+# read allele counts
+  alleleCounts <- fread('data/AlleleCounts.txt', header = T, check.names = F, data.table = F)
 
-# check blank wells to see if they are blank or not
-# excessive counts of allele counts meaning that they are not blank
-colSums(allele.count[, grep('blank', colnames(allele.count), ignore.case = T)])
-
-# remove unanchored snps and blank sample wells
-allele.count <- allele.count[(grep('UN', allele.count$CHR, ignore.case = T, invert = T)),
-                     (grep('BLANK', colnames(allele.count), ignore.case = T, invert = T))]
-colnames(allele.count) # check column names
-
-# print out allele.count colnames to see populations
-sort(colnames(allele.count)[-c(1:5)])
-
-# function to parse out populations
-parse_pop <- function(id=NULL, r.parent=NULL, s.parent=NULL) {
-  return(allele.count[, grep(paste(c(id, r.parent, s.parent), collapse = '|'), colnames(allele.count))])
-}
-
-# separate out the populations
-h3 <- parse_pop('CN_', 'Carol', 'Newton')
-h5 <- parse_pop('EN_', 'Erin', 'Newton')
-h6 <- parse_pop('FN_', 'Flynn', 'Newton')
-h10 <- parse_pop('JN_', 'Joy', 'Newton')
-h12 <- parse_pop('LN_', 'Lola', 'Newton')
-h13.newton <- parse_pop('MN_', 'Molly', 'Newton')
-h13.overley <- parse_pop('MO_', 'Molly', 'Overley')
-h26 <- parse_pop('KO_', 'KU2147', 'Overley')
-h32 <- parse_pop('SynOp', 'SyntheticW7984', 'OpataM85')
+## association tests
+  
+  # # Carol x Newton - H3
+  associationTest_AC(data = alleleCounts, pop.id = 'CN', gene = 'h3')
+  
+  # Erin x Newton - H5
+  associationTest_AC(data = alleleCounts, pop.id = 'EN', gene = 'h5')
+  
+  # Flynn x Newton - H6
+  associationTest_AC(data = alleleCounts, pop.id = 'FN', gene = 'h6')
+  
+  # Joy x Newton - H10
+  associationTest_AC(data = alleleCounts, pop.id = 'JN', gene = 'h10')
+  
+  # Lola x Newton - H12
+  associationTest_AC(data = alleleCounts, pop.id = 'LN', gene = 'h12')
+  
+  # Molly x Newton - H13
+  associationTest_AC(data = alleleCounts, pop.id = 'MN', gene = 'h13.newton')
+  
+  # Molly x Overley - H13
+  associationTest_AC(data = alleleCounts, pop.id = 'MO', gene = 'h13.overley')
+  
+  # KU2147 x Overley - H26
+  associationTest_AC(data = alleleCounts, pop.id = 'KO', gene = 'h26')
+  
+  # SynOP DH - H32
+  associationTest_AC(data = alleleCounts, pop.id = 'SynOp', gene = 'h32')
+  
