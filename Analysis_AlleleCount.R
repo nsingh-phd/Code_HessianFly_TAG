cf6f3269c0db23ea771b4e44408c750151533f2a
Narinder Singh <nss470@ksu.edu>
2018-10-03 13:41:08 -0500

    remove Av1 bait reads and analyze only Tv2 data because same results

--- Analysis_AlleleCount.R
+++ Analysis_AlleleCount.R
@@ -11,7 +11,6 @@
   alleleCounts <- fread('data/AlleleCounts.txt', header = T, check.names = F, data.table = F)
   
 # read allele counts from RenSeq Data
-  alleleCounts_RenSeq_Av1 <- fread('data/AC_RenSeq-Av1.txt', header = T, check.names = F, data.table = F)
   alleleCounts_RenSeq_Tv2 <- fread('data/AC_RenSeq-Tv2.txt', header = T, check.names = F, data.table = F)
   
 ## association tests
@@ -40,10 +39,6 @@
   ## Gene H26 families
       # KU2147 x Overley - H26
         associationTest_AC(data = alleleCounts, pop.id = 'KO', gene = 'h26')
-      # RenSeq Av1 Family 1 - KU2147 x Overley - H26
-        associationTest_AC(data = alleleCounts_RenSeq_Av1, pop.id = 'fam_1', gene = 'h26.renseq.av1.fam1')
-      # RenSeq Av1 Family 2 - KU2147 x Overley - H26
-        associationTest_AC(data = alleleCounts_RenSeq_Av1, pop.id = 'fam_2', gene = 'h26.renseq.av1.fam2')
       # RenSeq Tv2 Family 1 - KU2147 x Overley - H26
         associationTest_AC(data = alleleCounts_RenSeq_Tv2, pop.id = 'fam_1', gene = 'h26.renseq.tv2.fam1')
       # RenSeq Tv2 Family 2 - KU2147 x Overley - H26
