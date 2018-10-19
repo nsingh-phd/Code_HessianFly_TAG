3f0a6df7c40534fad74a605bfa97fc751855bb9e
Narinder Singh <nss470@ksu.edu>
2018-10-18 22:14:39 -0500

    minor aesthetic changes

--- Phenotye_plots.R
+++ Phenotye_plots.R
@@ -8,35 +8,32 @@
   source('functions.R')
 
 ## Hessian fly screening in greenhouse
-  pdf(file="output/phenotypic_distribution.pdf", width = 8.5, height = 11)
+  pdf(file="output/Fig.1_phenotypic_distribution.pdf", width = 8.5, height = 11)
   par(mfrow=c(4,2))
 
 ## H5
-  phenoPlots(c(18,63,65), "H5")
+  phenoPlots(values = c(18,63,65), name = "H5-EN", seg = "1:2:1", trait = 'HF')
 ## H6
-  phenoPlots(c(23,73,26), "H6")
+  phenoPlots(values = c(23,73,26), name = "H6-FN", seg = "1:2:1", trait = 'HF')
 ## H12
-  phenoPlots(c(7,46,20), "H12")
+  phenoPlots(values = c(7,46,20), name = "H12-LN", seg = "1:2:1", trait = 'HF')
 ## H13.Newton
-  phenoPlots(c(31,56,26), "H13 Newton")
+  phenoPlots(values = c(31,56,26), name = "H13-MN", seg = "1:2:1", trait = 'HF')
 ## H13.Overley
-  phenoPlots(c(19,39,29), "H13 Overley")
+  phenoPlots(values = c(19,39,29), name = "H13-MO", seg = "1:2:1", trait = 'HF')
 ## H26.Fam1
-  phenoPlots(c(68,177,71), "H26 Fam1")
+  phenoPlots(values = c(68,177,71), name = "H26-Fam1", seg = "1:2:1", trait = 'HF')
 ## H26.Fam2
-  phenoPlots(c(66,137,70), "H26 Fam2")
+  phenoPlots(values = c(66,137,70), name = "H26-Fam2", seg = "1:2:1", trait = 'HF')
 ## H32
-  phenoPlots(c(37,0,43), "H32")
+  phenoPlots(values = c(37,0,43), name = "H32-SynOpDH", seg = "1:1", trait = 'HF')
 
   dev.off()
 
 ## Plant color distribution in field
-  pdf(file="output/plant_color_field.pdf", width = 11, height = 5.5)
+  pdf(file="output/S1_HF_PlantColor.pdf", width = 11, height = 5.5)
   par(mfrow=c(1,2))
-  barplot(c(72,151,81), col = c('forestgreen', 'gray', 'green'), 
-          names.arg = c('Overley-like', 'Segregating', 'Ae. tauschii-like'), 
-          cex.axis = 1.5, cex.names = 1.2,
-          main = "Fam1 lines distribution for spike color")
+  phenoPlots(values = c(72,151,81), name = "H26-Fam1", seg = "1:2:1", trait = 'plant_color')
 
 # test of independence of color and hessian fly
   hfColData <- read.csv('data/HF_color_data.csv', header = T)
@@ -45,10 +42,9 @@
   
   chisq.test(categData)
   spineplot(categData, xaxlabels = c('Rr', 'RR', 'rr'), 
-            yaxlabels = c('Overley-like', 'Segregating', 'Ae. tauschii like'),
-            col = c('forestgreen', 'gray', 'green'), main = 'Color distribution within different genotypes')
+            yaxlabels = c("'Overley'-like", 'Het', 'Ae. tauschii like'),
+            col = c('forestgreen', 'gray', 'green'), 
+            main = 'Color distribution within different genotypes')
   mtext(paste('Chi-sq p-val =', round(chisq.test(categData)$p.value, 3)), side = 3, cex = 1.25)
   
   dev.off()
-
-  
